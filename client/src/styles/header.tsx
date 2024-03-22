07e128768987c6171d1aba0c658a4c3f041dd820
Mihnea <asmihnea5@gmail.com>
2024-03-22 17:45:02 +0200

    Rem

---
+++ client/src/styles/header.tsx
@@ -0,0 +1,9 @@
+import tw from "tailwind-styled-components"
+
+const HeaderDiv = tw.div`
+
+    
+
+`
+
+export default HeaderDiv
--- client/src/views/Home.view.tsx
+++ client/src/views/Home.view.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='h-96 w-auto bg-black text-center mx-auto max-w-[1280px]'>
+    <div className='h-96 w-auto bg-black text-center mx-auto max-w-[80rem]'>
         <h1 className='text-yellow-400'>
             Alearga Pentru Viata
         </h1>
