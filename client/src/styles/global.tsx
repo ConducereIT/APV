4559372970562e4a6aafa3d4285dedb7ea49ffd0
Mihnea <asmihnea5@gmail.com>
2024-03-22 12:32:29 +0200

    fixed

--- client/src/styles/global.tsx
+++ client/src/styles/global.tsx
@@ -2,44 +2,36 @@ import { createGlobalStyle, withTheme } from "styled-components";
 
 import { ThemeProps } from "./themes";
 
-import tw from "twin.macro";
+// import tw from "tailwind-styled-components"
 
 
 type GlobalThemeProps = {
     theme: ThemeProps;
   };
 
-const GlobalStyle = createGlobalStyle`
+
+const globalStyles = createGlobalStyle`
   :root {
     --dark-background: #1A1B27;
     --dark-text: #F5F5F7;
 
     --light-background: #f2f2f2;
     --light-text: #2E0509;
-
   }
 
 
-  ${tw`m-0 p-0 box-border outline-none`}
   
   body  {
     -webkit-font-smoothing: antialiased;
-    height: 100vh;
-    width: 50vw;
-    margin: 0 auto;
     background-color: ${({ theme }: GlobalThemeProps) => theme.background};
-    display: flex;
-    justify-content: center;
-    align-items: center;
   }
 
   h1 {
-    font-size: 3.375rem;
     color: ${({ theme }: GlobalThemeProps) => theme.text};
   }
 
 `;
 
+  const GlobalStyle = withTheme(globalStyles)
 
-
-export default withTheme(GlobalStyle);
+export default GlobalStyle;
