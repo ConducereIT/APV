82f833e3e5f665392569280ec697ff29e6f657b5
ania047 <Ana-Maria@DESKTOP-S6J4G00>
2024-03-27 19:48:35 +0200

    add-ceva

---
+++ client/src/components/AboutText.component.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function AboutText() {
+  return (
+    <div className='mt-12 flex justify-between mx-10'>
+        <div className='w-1/2'>
+        <h1>
+            CE ESTE ALEARGĂ PENTRU VIAȚĂ?
+        </h1>
+        <p> Având o tradiție de peste 13 ani, Aleargă Pentru Viață reprezintă un cros caritabil organizat de Liga Studenților Electroniști. Acesta este format din mai multe curse împărțite pe categorii de vârstă, la care participarea se face pe baza unei donații pentru o cauză umanitară.
+Astfel, scopul evenimentului Aleargă Pentru Viață este de a da o mână de ajutor unei persoane care se confruntă cu o situație dificilă din punct de vedere medical, reușind să aducă un aport semnificativ pentru revenirea la o viață normală a acesteia.
+Prin intermediul acestui eveniment reușim atât să îi ajutăm pe cei aflați în dificultate, cât și să promovăm mișcarea și un stil de viață sănătos în rândul oamenilor din jurul nostru.
+        </p>
+        <p>cecva</p>
+
+        </div>
+
+        <div>
+            <p>orice</p>
+        </div>
+        
+    </div>
+  )
+}
