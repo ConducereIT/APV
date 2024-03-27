14841371ffb000c0222ea07f46fe4ef9f845404e
Mihnea <asmihnea5@gmail.com>
2024-03-27 23:35:10 +0200

    Done

--- client/src/components/Curse.component.tsx
+++ client/src/components/Curse.component.tsx
@@ -2,7 +2,7 @@ import { CurseValide } from "../views/Curse.view";
 
 const CurseComponent: React.FC<CurseValide> = ({ interfata}): JSX.Element => {
 
-    const { cursa, oraStart, distanta, donatieMinima, descriere } = interfata || {};
+    const { cursa, oraStart, distanta, donatieMinima, descriere, urlHarta } = interfata || {};
     
     return (
         <div className="ml-8 pt-5">
@@ -23,7 +23,7 @@ const CurseComponent: React.FC<CurseValide> = ({ interfata}): JSX.Element => {
                         <h1 className="font-bolt text-xl mt-8"><span className="font-normal">Pentru mai multe informații consultați </span> regulamentul <span className="font-normal"> ! </span> </h1>
 
                         <div className="mt-10 h-[30rem] ml-1 w-1/2 ">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m52!1m12!1m3!1d1359.0739952763695!2d26.051536155519436!3d44.4381565085272!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m37!3e2!4m3!3m2!1d44.4372435!2d26.047853099999998!4m3!3m2!1d44.4392806!2d26.047387!4m3!3m2!1d44.4388363!2d26.0495043!4m3!3m2!1d44.438002399999995!2d26.051835699999998!4m3!3m2!1d44.440619999999996!2d26.0526777!4m3!3m2!1d44.4378836!2d26.054248899999997!4m3!3m2!1d44.4377256!2d26.051095999999998!4m3!3m2!1d44.4380264!2d26.0493903!4m3!3m2!1d44.4372292!2d26.0480509!5e0!3m2!1sro!2sro!4v1679944496994!5m2!1sro!2sro" loading="lazy" className="border-0 h-full w-full" title={`${cursa}`} ></iframe>
+                            <iframe src={`${urlHarta}`} loading="lazy" className="border-0 h-full w-full" title={`${cursa}`} ></iframe>
                         </div>
 
                         <br className=" mt-3" />
