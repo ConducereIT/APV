d590ddfa598c4afdb81642fb443b45b4f9dfff46
ania047 <Ana-Maria@DESKTOP-S6J4G00>
2024-04-12 03:11:13 +0300

    login

--- client/src/components/AboutText.component.tsx
+++ client/src/components/AboutText.component.tsx
@@ -9,21 +9,21 @@ export default function AboutText() {
         <h2 className="md:text-5xl font-barlow-condensed leading-normal font-bold md:mt-40">
           CE ESTE ALEARGĂ PENTRU VIAȚĂ?
         </h2>
-        <p className="text-sm md:text-xl font-barlow font-normal leading-normal text-custom-gray mt-8 md:mt-12">
+        <p className="text-sm md:text-base font-barlow font-normal leading-normal text-custom-gray mt-8 md:mt-12">
           Având o tradiție de peste 14 ani, Aleargă Pentru Viață reprezintă un
           cros caritabil organizat de Liga Studenților Electroniști. Acesta este
           format din mai multe curse împărțite pe categorii de vârstă, la care
           participarea se face pe baza unei donații pentru o cauză umanitară.
         </p>
 
-        <p className="text-sm md:text-xl font-barlow font-normal leading-normal text-custom-gray mt-4 md:mt-6">
+        <p className="text-sm md:text-base font-barlow font-normal leading-normal text-custom-gray mt-4 md:mt-6">
           Astfel, scopul evenimentului Aleargă Pentru Viață este de a da o mână
           de ajutor unei persoane care se confruntă cu o situație dificilă din
           punct de vedere medical, reușind să aducă un aport semnificativ pentru
           revenirea la o viață normală a acesteia.
         </p>
 
-        <p className="text-sm md:text-xl font-barlow font-normal leading-normal text-custom-gray mt-4 md:mt-6">
+        <p className="text-sm md:text-base font-barlow font-normal leading-normal text-custom-gray mt-4 md:mt-6">
           Prin intermediul acestui eveniment reușim atât să îi ajutăm pe cei
           aflați în dificultate, cât și să promovăm mișcarea și un stil de viață
           sănătos în rândul oamenilor din jurul nostru.
