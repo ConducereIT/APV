6e80909e57586e81d90fecb036409c5d2247d679
Cristi Miloiu <miloiuc4@gmail.com>
2024-04-12 19:39:18 +0300

    .

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,12 +7,12 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import { AuthService } from '@genezio/auth';
 
 AuthService.getInstance().setTokenAndRegion(
-"0-7wbqm5eaw3w6pkcauqnk4chrmu0zyron",
+"0-ul63zxst4qppvf6bbn526l2uyi0arumm",
  "eu-central-1");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId='30633693522-jd2bijo2t8drg37f1l9jngmmupdj3015.apps.googleusercontent.com'>
+    <GoogleOAuthProvider  clientId='30633693522-jd2bijo2t8drg37f1l9jngmmupdj3015.apps.googleusercontent.com'>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
