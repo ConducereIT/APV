aa0cdc3005a194444d1c492a31b42cafbdba0ab8
MDGcode <adrianboscanici@yahoo.com>
2024-04-22 11:51:36 +0300

    view users

--- client/src/views/Checkin.view.tsx
+++ client/src/views/Checkin.view.tsx
@@ -1,7 +1,10 @@
-import {BackendService} from "@genezio-sdk/apv-production";
-import {AuthService} from "@genezio/auth";
+import React, { useState } from "react";
+import { BackendService } from "@genezio-sdk/apv-production";
+import { AuthService } from "@genezio/auth";
 
 const Checkin: React.FC = () => {
+  const [users, setUsers] = useState<any[]>([]);
+  const [render, setRender] = useState(false);
 
   const handleFunctionShowCurrentUser = async () => {
     try {
@@ -16,44 +19,110 @@ const Checkin: React.FC = () => {
     try {
       const response = await BackendService.getAllRaces();
       console.log(response);
+      setUsers(response);
+      setRender(true);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const changeCustomDetailsUserByUserId = async () => {
     try {
-      const response = await BackendService.updateUserDetailsByUserId("a23efdde-3112-44ae-a98a-92f723c2b10f", {
-        salut:"Salut",
-        checkin: true,
-        malai:"200lei",
-      });
+      const response = await BackendService.updateUserDetailsByUserId(
+        "a23efdde-3112-44ae-a98a-92f723c2b10f",
+        {
+          salut: "Salut",
+          checkin: true,
+          malai: "200lei",
+        }
+      );
       console.log(response);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
-    <>
-      <div className="mt-20 mb-10 flex flex-col items-center space-y-3">
-        <h1>Poti vedea rezultatele in consola</h1>
-        <button onClick={handleFunctionShowCurrentUser}
-                className="bg-[#1b756f] hover:bg-[#3f9892]  h-[20%] rounded-md text-white text-base text-bold w-[10%]">
-          show current user
-        </button>
+    <div className="mt-20 mb-10 flex flex-col items-center space-y-3">
+      <h1>Poti vedea rezultatele in consola</h1>
+      <button
+        onClick={handleFunctionShowCurrentUser}
+        className="bg-[#1b756f] hover:bg-[#3f9892] h-[10%] rounded-md text-white text-base font-bold w-[20%]"
+      >
+        Show Current User
+      </button>
+      <button
+        onClick={changeCustomDetailsUserByUserId}
+        className="bg-[#1b756f] hover:bg-[#3f9892] h-[10%] rounded-md text-white text-base font-bold w-[20%]"
+      >
+        Update User
+      </button>
+      <button
+        onClick={handleAllCursa}
+        className="bg-[#1b756f] hover:bg-[#3f9892] h-[10%] rounded-md text-white text-base font-bold w-[20%]"
+      >
+        All Curse
+      </button>
 
-        <button onClick={changeCustomDetailsUserByUserId}
-                className="bg-[#1b756f] hover:bg-[#3f9892]  h-[20%] rounded-md text-white text-base text-bold w-[10%]">
-          update user
-        </button>
-
-        <button onClick={handleAllCursa}
-                className="bg-[#1b756f] hover:bg-[#3f9892]  h-[20%] rounded-md text-white text-base text-bold w-[10%]">
-          all curse
-        </button>
-      </div>
-    </>
+      {render && (
+        <div className="grid grid-cols-7 gap-2 mx-12 mt-24">
+          <div className="font-bold">Id</div>
+          <div className="font-bold">Nume</div>
+          <div className="font-bold">Telefon</div>
+          <div className="font-bold">Marime Tricou</div>
+          <div className="font-bold">Numar Tricou</div>
+          <div className="font-bold">Metoda Plata</div>
+          <div className="font-bold">Timp</div>
+          {users.map((user, index) => (
+            <React.Fragment key={index}>
+              <div>{user.id}</div>
+              <div>
+                {user.name != "" && user.name != null ? (
+                  user.name
+                ) : (
+                  <span className="text-red-600">null</span>
+                )}
+              </div>
+              <div>
+                {user.phone != "" && user.phone != null ? (
+                  user.phone
+                ) : (
+                  <span className="text-red-600">null</span>
+                )}
+              </div>
+              <div>
+                {user.marimeTricou != "" && user.marimeTricou != null ? (
+                  user.marimeTricou
+                ) : (
+                  <span className="text-red-600">null</span>
+                )}
+              </div>
+              <div>
+                {user.numarTricou != null ? (
+                  user.numarTricou
+                ) : (
+                  <span className="text-red-600">null</span>
+                )}
+              </div>
+              <div>
+                {user.revolute_cash != "" && user.revolute_cash != null ? (
+                  user.revolute_cash
+                ) : (
+                  <span className="text-red-600">null</span>
+                )}
+              </div>
+              <div>
+                {user.timpAlergat != null ? (
+                  user.timpAlergat
+                ) : (
+                  <span className="text-red-600">null</span>
+                )}
+              </div>
+            </React.Fragment>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
