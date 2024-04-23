35b638ba62df3175c7278f529b8bd31d29dcfc84
Mihnea <asmihnea5@gmail.com>
2024-04-23 23:23:17 +0300

    Send Race Completion Email

--- server/mailer.ts
+++ server/mailer.ts
@@ -84,5 +84,56 @@ export class Mailer{
         } 
     }
 
+    async sendRaceCompletionEmail(
+        to: string,
+        subject: string,
+        nume: string,
+        cursa: string,
+        minuteAlergate: string
+    ){
+        const html = `<!doctype html>
+        <html lang="ro">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Aleargă pentru Viață - Ediția a XV-a</title>
+        </head>
+        <body style="margin: 0; padding: 0; font-family: Arial, sans-serif; background-color: #f4f4f4;">
+        <div style="max-width: 600px; margin: 20px auto; background-color: #081043; padding: 30px; border-radius: 10px; border: 4px solid #081043;">
+            <div style="text-align: center;">
+                    <a href="https://ibb.co/g9SP8mx"><img src="https://i.ibb.co/H4txyBj/logo-apv-XV.png" alt="Logo Aleargă pentru Viață" style="max-width: 75%; height: auto; display: inline-block; box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);"></a>
+            </div>
+            <h1 style="color: #fff; text-align: center; margin-bottom: 20px;">Felicitari, ${nume}!</h1>
+            <p style="color: #fff; text-align: center; font-size: 14px;">Detalii ${cursa}, ediția a XV-a!</p>
+            <div style="background-color: #2E86AA; padding: 15px; border-radius: 5px; margin-top: 30px; margin-bottom: 30px; text-align: center;">
+            <h2 style="color: #fff; font-size: 18px; margin-top: 0;">Ai terminat ${cursa} în ${minuteAlergate} de minute!</h2>
+        </div>        
+            <p style="color: #fff; text-align: center; font-size: 16px;">Iti multumim ca ai alergat alaturi de noi pentru o cauză nobilă!</p>
+            <p style="color: #fff; text-align: center; font-size: 16px;">Cu drag,</p>
+            <p style="color: #fff; text-align: center; font-size: 16px;">Echipa "Aleargă Pentru Viață"</p>
+            <p style="color: #fff; text-align: center; font-size: 16px; margin-bottom: 0;"><a href="https://apv.lsebucuresti.org/" style="text-decoration: none; color: #007bff;">https://apv.lsebucuresti.org/</a></p>
+        </div>
+        </body>
+        </html>`;
+
+        const inlineHtml = juice(html);
+
+        
+        try {
+            await this.transporter.sendMail({
+                from: process.env.SEND_MAIL_USER,
+                to,
+                subject,
+                html: inlineHtml,
+            });
+
+            return true;
+        } catch (error) {
+            console.error("Error sending email:", error);
+            return false;
+        } 
+    }
 
 }
