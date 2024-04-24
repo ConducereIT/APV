0f7814184476fcd0bb5ad735ab03af15eca19342
ania047 <Ana-Maria@DESKTOP-S6J4G00>
2024-04-24 20:20:47 +0300

    update istoric

--- client/src/config/IstoricConfig.ts
+++ client/src/config/IstoricConfig.ts
@@ -13,18 +13,18 @@ const carouselItems = [
   {
     photo: subject3,
     text:
-      "În cea de a unsprezecea ediție, l-am cunoscut pe Eduard Sobaru, un suflet tânăr, ambițios, care inspiră viață și bunătate și a fost dintotdeauna un spirit liber și aventuros. Eduard lucra în parcul de aventură Edenland, după o operațiune de salvare a unor copii, echipamentul lui de siguranță s-a desprins și a căzut în gol de la o înălțime de 12 metri. Eduard a fost diagnosticat cu tetraplegie spastică, urmând un program riguros de recuperare compus din ședințe de kinetoterapie individuală, terapie, masaj și consiliere psihologică.",
+      "În cea de a unsprezecea ediție, l-am cunoscut pe Eduard Sobaru, un suflet tânăr, ambițios, care inspiră viață și bunătate și a fost dintotdeauna un spirit liber și aventuros. Eduard lucra în parcul de aventură Edenland, după o operațiune de salvare a unor copii, echipamentul lui de siguranță s-a desprins și a căzut în gol de la o înălțime de 12 metri. Eduard a fost diagnosticat cu tetrapareza spastică, urmând un program riguros de recuperare compus din ședințe de kinetoterapie individuală, terapie, masaj și consiliere psihologică.",
     name: "Eduard Sobaru",
   },
   {
     photo: subject4,
     text:
-      "Penultima ediție ni-l scoate în cale pe Darius Ghereben, student, fost handbalist și un tânăr care avea tot viitorul în față, întotdeauna zâmbitor.  Darius a suferit un accident în timp ce mergea cu ATV-ul, în urma căruia și-a fracturat vertebrele C5-C6, rămânând paralizat de la gât în jos. În urma unei operații la coloană doctorii au reușit sa îi ofere o nouă șansă, însă tratamentul a fost foarte costisitor.",
+      "Penultima ediție ni-l scoate în cale pe Darius Ghereben, student, fost handbalist și un tânăr care avea tot viitorul în față, întotdeauna zâmbitor. Darius a suferit un accident în timp ce mergea cu ATV-ul, în urma căruia și-a fracturat vertebrele C5-C6, rămânând paralizat de la gât în jos. În urma unei operații la coloană doctorii au reușit să îi ofere o nouă șansă, însă tratamentul a fost foarte costisitor.",
     name: "Darius Ghereben",
   },
   {
     photo: subject1,
-    text: "Anul trecut, am sprijinit povestea lui Andrei Bucur, un tânăr ambițios, fost student la Academia de Poliție. În timpul unui antrenament pentru cariera sa visată, la data de 3 mai 2017, a suferit un leșin și a fost diagnosticat cu un anevrism cerebral. După numeroase intervenții chirurgicale costisitoare în Franța, Andrei a scăpat de pericolul decesului, dar rămâne cu semipareză facială. șansele sale de recuperare totală sunt mari, dar tratamentele au fost extrem de costisitoare pentru el și familia sa.",
+    text: "Anul trecut, am sprijinit povestea lui Andrei Bucur, un tânăr ambițios, fost student la Academia de Poliție. În timpul unui antrenament pentru cariera sa la care visa, pe data de 3 mai 2017, a suferit un leșin și a fost diagnosticat cu un anevrism cerebral. După numeroase intervenții chirurgicale costisitoare în Franța, Andrei a scăpat de pericolul decesului, dar rămâne cu semipareză facială. Șansele sale de recuperare totală sunt mari, dar tratamentele au fost extrem de costisitoare pentru el și familia sa.",
     name: "Andrei Bucur",
   },
 ];
