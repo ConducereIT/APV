977348b9377861d2c036fe8b7d80f9521b6f5cac
ania047 <Ana-Maria@DESKTOP-S6J4G00>
2024-05-02 21:43:47 +0300

    all sponsors

--- client/src/config/SponsorsConfig.ts
+++ client/src/config/SponsorsConfig.ts
@@ -1,6 +1,54 @@
-import ALBUME from "../assets/sponsors/sponsor1.webp";
+import AZUGA from "../assets/sponsors/sponsor1.webp";
+import VERONA from "../assets/sponsors/sponsor2.webp";
+import NAN from "../assets/sponsors/sponsor3.webp";
+import MARCO from "../assets/sponsors/sponsor4.webp";
+import LYCOPEN from "../assets/sponsors/sponsor5.webp";
+import KARA from "../assets/sponsors/sponsor6.webp";
+import IVELO from "../assets/sponsors/sponsor7.webp";
+import ISOSTAR from "../assets/sponsors/sponsor8.webp";
+import GSMNET from "../assets/sponsors/sponsor9.webp";
+import FSSU from "../assets/sponsors/sponsor10.webp";
+import FITMOTION from "../assets/sponsors/sponsor11.webp";
+import BALMIX from "../assets/sponsors/sponsor12.webp";
+import ALBUME from "../assets/sponsors/sponsor13.webp";
 
 export const sponsori = [
+    {
+        image: AZUGA,
+    }, 
+    {
+        image: VERONA,
+    }, 
+    {
+        image: NAN,
+    }, 
+    {
+        image: MARCO,
+    }, 
+    {
+        image: LYCOPEN,
+    }, 
+    {
+        image: KARA,
+    }, 
+    {
+        image: IVELO,
+    }, 
+    {
+        image: ISOSTAR,
+    }, 
+    {
+        image: GSMNET,
+    }, 
+    {
+        image: FSSU,
+    }, 
+    {
+        image: FITMOTION,
+    }, 
+    {
+        image: BALMIX,
+    }, 
     {
         image: ALBUME,
     }, 
