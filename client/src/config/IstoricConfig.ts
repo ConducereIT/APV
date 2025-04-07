618619432d1343dccd3be212a3ed1f1bb5cb72dc
CristiSx <cristiansalafenco@gmail.com>
2025-04-07 20:49:51 +0300

    schimbari text

--- client/src/config/IstoricConfig.ts
+++ client/src/config/IstoricConfig.ts
@@ -8,13 +8,13 @@ const carouselItems = [
   {
     photo: subject2,
     text:
-      "În cea de a zecea ediție ne-am întâlnit cu Valentina Cotolea, care este o tânără dornică de viață, plănuieşte să înființeze un centru de recuperare pentru adulți, pentru a-i ajuta pe cei cu probleme. Însă chiar ea a fost încercată de destin încă de la naștere, fiind diagnosticată cu parapareză spastică.",
+      "În cea de-a zecea ediție ne-am întâlnit cu Valentina Cotolea, care este o tânără dornică de viață, plănuieşte să înființeze un centru de recuperare pentru adulți, pentru a-i ajuta pe cei cu probleme. Însă chiar ea a fost încercată de destin încă de la naștere, fiind diagnosticată cu parapareză spastică.",
     name: "Valentina Cotolea",
   },
   {
     photo: subject3,
     text:
-      "În cea de a unsprezecea ediție, l-am cunoscut pe Eduard Sobaru, un suflet tânăr, ambițios, care inspiră viață și bunătate și a fost dintotdeauna un spirit liber și aventuros. Eduard lucra în parcul de aventură Edenland, după o operațiune de salvare a unor copii, echipamentul lui de siguranță s-a desprins și a căzut în gol de la o înălțime de 12 metri. Eduard a fost diagnosticat cu tetrapareza spastică, urmând un program riguros de recuperare compus din ședințe de kinetoterapie individuală, terapie, masaj și consiliere psihologică.",
+      "În cea de-a unsprezecea ediție, l-am cunoscut pe Eduard Sobaru, un suflet tânăr, ambițios, care inspiră viață și bunătate și a fost dintotdeauna un spirit liber și aventuros. Eduard lucra în parcul de aventură Edenland, după o operațiune de salvare a unor copii, echipamentul lui de siguranță s-a desprins și a căzut în gol de la o înălțime de 12 metri. Eduard a fost diagnosticat cu tetrapareza spastică, urmând un program riguros de recuperare compus din ședințe de kinetoterapie individuală, terapie, masaj și consiliere psihologică.",
     name: "Eduard Sobaru",
   },
   {
@@ -25,7 +25,7 @@ const carouselItems = [
   },
   {
     photo: subject1,
-    text: "Penultima ediție, am sprijinit povestea lui Andrei Bucur, un tânăr ambițios, fost student la Academia de Poliție. În timpul unui antrenament pentru cariera sa la care visa, pe data de 3 mai 2017, a suferit un leșin și a fost diagnosticat cu un anevrism cerebral. După numeroase intervenții chirurgicale costisitoare în Franța, Andrei a scăpat de pericolul decesului, dar rămâne cu semipareză facială. Șansele sale de recuperare totală...",
+    text: "Cea de-a treisprezecea ediție, am sprijinit povestea lui Andrei Bucur, un tânăr ambițios, fost student la Academia de Poliție. În timpul unui antrenament pentru cariera sa la care visa, pe data de 3 mai 2017, a suferit un leșin și a fost diagnosticat cu un anevrism cerebral. După numeroase intervenții chirurgicale costisitoare în Franța, Andrei a scăpat de pericolul decesului, dar rămâne cu semipareză facială. Șansele sale de recuperare totală...",
     name: "Andrei Bucur",
   },
   {
